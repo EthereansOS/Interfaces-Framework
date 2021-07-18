ec7e820ab84f85ce1327aaba4b4b3b027acf290b
gc <noreply@remotamente.it>
2021-07-18 17:48:43 +0200

    feat: add liquidity and inflation values in rules and funds

--- packages/organization-plugin/src/components/RulesAndFunds/index.js
+++ packages/organization-plugin/src/components/RulesAndFunds/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Card, Typography, Chip, Link } from '@dfohub/design-system'
 import {
   blockchainCall,
+  fromDecimals,
   tokenPercentage,
   useEthosContext,
   useWeb3,
@@ -10,14 +11,19 @@ import { useLocation } from 'react-router-dom'
 
 import Section from '../shared/Section'
 import { OrganizationPropType } from '../../propTypes'
+import useOrganizations from '../../hooks/useOrganizations'
 
 import style from './rules-and-funds.module.scss'
 
 function RulesAndFunds({ organization }) {
   const { web3 } = useWeb3()
   const context = useEthosContext()
-  const [blocks, setBlocks] = useState()
   const location = useLocation()
+  const { dfoHub } = useOrganizations()
+
+  const [blocks, setBlocks] = useState()
+  const [inflationStatus, setInflationStatus] = useState()
+  const [liquidityStatus, setLiquidityStatus] = useState()
 
   useEffect(() => {
     const fetchBlocks = async () => {
@@ -27,13 +33,67 @@ function RulesAndFunds({ organization }) {
         'getMinimumBlockNumberForSurvey',
         '0x'
       )
-      console.log(blocksCall)
 
       setBlocks(web3.eth.abi.decodeParameters(['uint256'], blocksCall)[0])
     }
 
     fetchBlocks()
   }, [context, organization.dFO.methods.read, web3])
+
+  useEffect(() => {
+    const fetchInflationAndLiquidityStatus = async () => {
+      const inflation = await blockchainCall(
+        { web3, context },
+        organization.stateHolder.methods.getUint256,
+        'fixedInflation.transfers.length'
+      )
+      let liquidity = 'Loading'
+
+      try {
+        let json = await blockchainCall(
+          { web3, context },
+          organization.stateHolder.methods.toJSON
+        )
+
+        json = JSON.parse(
+          json.endsWith(',]')
+            ? json.substring(0, json.lastIndexOf(',]')) + ']'
+            : json
+        )
+        for (let i in json) {
+          const element = json[i]
+          const methodName =
+            'get' +
+            element.type.substring(0, 1).toUpperCase() +
+            element.type.substring(1)
+          element.value = await blockchainCall(
+            { web3, context },
+            organization.stateHolder.methods[methodName],
+            element.name
+          )
+          if (
+            element.name.indexOf('staking.transfer.authorized.') === 0 &&
+            element.value
+          ) {
+            liquidity = 'Active'
+          }
+
+          if (inflation === 'Active' && liquidity === 'Active') {
+            break
+          }
+        }
+      } catch (e) {
+        console.log('error fetching liquidity', e)
+      }
+
+      setInflationStatus(inflation > 0 ? 'Active' : 'Not set')
+      setLiquidityStatus(liquidity)
+    }
+
+    if (organization?.stateHolder) {
+      fetchInflationAndLiquidityStatus()
+    }
+  }, [context, organization.stateHolder, web3])
 
   return (
     <Card>
@@ -53,27 +113,46 @@ function RulesAndFunds({ organization }) {
         </Section>
         <Section category="🧮 Assets:" className={style.assets} column>
           <Typography variant="body2" className={style.assetsText}>
-            {/* TODO */}
             <strong>
               {tokenPercentage(
                 organization?.communityTokens,
                 organization?.totalSupply
               )}
             </strong>{' '}
-            (<strong>{organization?.communityTokens}</strong>{' '}
+            (
+            <strong>
+              {fromDecimals(
+                organization?.communityTokens,
+                organization?.decimals
+              )}
+            </strong>{' '}
             {organization?.symbol})
-            <br /> <strong>{organization?.walletETH}</strong> ETH
+            <br /> <strong>
+              {fromDecimals(organization?.walletETH, 18)}
+            </strong>{' '}
+            ETH
             <br />
-            <strong>{organization?.walletUSDC}</strong> USDC
+            <strong>{fromDecimals(organization?.walletUSDC, 6)}</strong> USDC
             <br />
-            <strong>{organization?.walletDAI}</strong> DAI
+            <strong>{fromDecimals(organization?.walletDAI, 18)}</strong> DAI
+            {organization.key !== 'DFO' && (
+              <>
+                <br />
+                <strong>
+                  {fromDecimals(organization?.walletBUIDL, dfoHub?.decimals)}
+                </strong>{' '}
+                BUIDL
+              </>
+            )}
           </Typography>
           <Link to={`${location?.pathname}/defi/wallet`}>
             <Chip className={style.chip} size="small" label="View all" />
           </Link>
         </Section>
-        <Section category="🖨 Fixed Inflation:">
-          {/* TODO */}
+        <Section column category="🖨 Fixed Inflation:">
+          {inflationStatus && (
+            <Typography variant="body2">{inflationStatus}</Typography>
+          )}
           <Link to={`${location?.pathname}/defi/farming`}>
             <Chip className={style.chip} size="small" label="More" />
           </Link>
@@ -92,8 +171,10 @@ function RulesAndFunds({ organization }) {
             </Link>
           </div>
         </Section>
-        <Section category="🦄 Liquidity Mining:">
-          {/* TODO */}
+        <Section column category="🦄 Liquidity Mining:">
+          {liquidityStatus && (
+            <Typography variant="body2">{liquidityStatus}</Typography>
+          )}
           <Link to={`${location?.pathname}/defi/farming`}>
             <Chip className={style.chip} size="small" label="More" />
           </Link>
