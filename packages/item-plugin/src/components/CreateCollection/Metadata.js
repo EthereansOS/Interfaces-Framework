97f7ff4122fb102373a0b70cc6ba917aa6195c84
Davide Fiorello <davide@codeflyer.com>
2021-07-23 09:14:51 +0200

    feat(collection): add field validation

--- packages/item-plugin/src/components/CreateCollection/Metadata.js
+++ packages/item-plugin/src/components/CreateCollection/Metadata.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Formik,
   Form,
   EditField,
   FileField,
   ColorField,
+  Yup,
 } from '@dfohub/components'
 import { Button, Select, Typography } from '@dfohub/design-system'
-import * as Yup from 'yup'
 import T from 'prop-types'
 
-const MetadataSchema = Yup.object().shape({
-  // host: Yup.string().required('mmm... Your Collection deserves a name'),
-  // hostAddress: Yup.string().when('host', {
-  //   is: 'wallet',
-  //   then: Yup.string().required('Must enter address'),
-  // }),
+const MetadataSchemaBasic = Yup.object().shape({
+  description: Yup.string().required(`Description is mandatory`),
+  background_color: Yup.string().required(`Background color is mandatory`),
+  discussionUri: Yup.string().isValidUrl(`Discussion Link must be a valid URL`),
+  externalDNS: Yup.string().isValidUrl(`DNS Link must be a valid URL`),
+  externalENS: Yup.string().isValidUrl(`ENS Link must be a valid URL`),
+  repoUri: Yup.string().isValidUrl(`Repo Link must be a valid URL`),
+  image: Yup.mixed()
+    .imageMaxWidth(2000)
+    .imageMaxMBSize(0.1)
+    .required('mmm... Your Collection deserves a name'),
 })
 
+const MetadataSchemaCustom = Yup.object().shape({
+  description: Yup.string().required(`Description is mandatory`),
+  background_color: Yup.string().required(`Background color is mandatory`),
+  discussionUri: Yup.string().isValidUrl(`Discussion Link must be a valid URL`),
+})
 const metadataTypeOptions = [
   { id: 'basic', label: 'Basic' },
   { id: 'custom', label: 'Custom' },
@@ -27,28 +37,28 @@ const MetadataBasic = () => {
   return (
     <>
       <EditField
-        id="metadataDescription"
-        name="metadataDescription"
+        id="description"
+        name="description"
         label="Description *"
         description="A description of the collection"
         isMultiline
         rows="4"
       />
       <EditField
-        id="metadataDiscussionLink"
-        name="metadataDiscussionLink"
+        id="discussionUri"
+        name="discussionUri"
         label="Discussion Link"
         description="A link to a social hub and/or discussion channel for the collection (if any)"
       />
       <EditField
-        id="metadataDnsLink"
-        name="metadataDnsLink"
+        id="externalDNS"
+        name="externalDNS"
         label="DNS Link"
         description="A link to the official website of this project (if any)"
       />
       <EditField
-        id="metadataEnsLink"
-        name="metadataEnsLink"
+        id="externalENS"
+        name="externalENS"
         label="ENS Link"
         description="An ENS link to the official website of this project (if any)"
       />
@@ -59,29 +69,29 @@ const MetadataBasic = () => {
         description="A link to the official repo of this project (if any)"
       />
       <FileField
-        id="metadataCoverRegular"
-        name="metadataCoverRegular"
+        id="image"
+        name="image"
         label="Cover (Regular) * "
         description="The cover img must be .png or .gif and at max 5mb length, due to users experience in IPFS download speed limitations (recommended size 350 x 350)"
       />
       <FileField
-        id="metadataCoverHigh"
-        name="metadataCoverHigh"
+        id="image_data"
+        name="image_data"
         label="Cover (High Quality)"
         description="No limitations for the HQ version of the image"
       />
+      <ColorField
+        id="background_color"
+        name="background_color"
+        label="Background Color *"
+        description="The background color behind your cover used in most dApps, if not fixed with their standard image sizes"
+      />
       <FileField
-        id="metadataLicence"
-        name="metadataLicence"
+        id="licence_url"
+        name="licence_url"
         label="Licence File"
         accept=".pdf,.html,.md,.txt"
         description="A file that represents the legal licence of all of ITEMs in the collection (if any). Accepted file types: .pdf, .html, .md, .txt"
-      />
-      <ColorField
-        id="metadataBackgroundColor"
-        name="metadataBackgroundColor"
-        label="Background Color *"
-        description="The background color behind your cover used in most dApps, if not fixed with their standard image sizes"
       />
     </>
   )
@@ -106,6 +116,7 @@ const MetadataCustom = () => {
 }
 
 const Metadata = ({ onNext, onBack, values }) => {
+  const [type, setType] = useState('basic')
   const handleSubmit = (props) => {
     onNext(props, 'granularity')
   }
@@ -113,11 +124,14 @@ const Metadata = ({ onNext, onBack, values }) => {
   return (
     <section>
       <Formik
+        validateOnChange={false}
         initialValues={{
           metadataType: values.metadataType || 'basic',
           metadataBackgroundColor: values.metadataBackgroundColor || '#c00',
         }}
-        validationSchema={MetadataSchema}
+        validationSchema={
+          type === 'basic' ? MetadataSchemaBasic : MetadataSchemaCustom
+        }
         onSubmit={handleSubmit}>
         {({ isValid, setFieldValue, values }) => (
           <Form>
@@ -126,6 +140,7 @@ const Metadata = ({ onNext, onBack, values }) => {
               id="metadataType"
               options={metadataTypeOptions} // TODO we gotta fetch the tokens for the select
               onSelect={(id, value) => {
+                setType(value)
                 setFieldValue(id, value)
               }}
               value={values.metadataType}
